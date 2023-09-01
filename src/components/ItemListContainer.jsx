7ef74d892e4d02d343426d9b3b0526d6e4002be7
solano de zuasnabar <sdezuasnabar@gmail.com>
2023-09-01 18:57:33 -0300

    vercel.json re-creado

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,7 +3,6 @@ import ItemList from './ItemList'
 import { useParams } from 'react-router-dom'
 import { collection, getDocs, query, where } from 'firebase/firestore'
 import { db } from '../firebase/config'
-
 const ItemListContainer = () => {
 
     const [productos, setProductos] = useState([])
